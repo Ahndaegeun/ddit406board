65c662db57c090a23f68d0c4d8b024978ea98dfe
kade <adg0807@naver.com>
2021-07-26 13:26:17 +0900

    media query ing

--- js/index.js
+++ js/index.js
@@ -2,6 +2,12 @@ const commentBtn = document.querySelector('.comment-btn')
 const commentList = document.querySelector('.comment-list')
 const modifyBtn = document.querySelector('.user-modify-btn')
 const userInfo = document.querySelector('.user-modify-info')
+const mobileMenu = document.querySelector('.mobile-device-menu')
+const mobileContentsBtn = document.querySelector('.mobile-contents-btn')
+const mobileInfoBtn = document.querySelector('.mobile-info-btn')
+
+const userInfo = document.querySelector('.user-info')
+const board = document.querySelector('main')
 
 commentBtn.addEventListener('click', e => {
   showCommentList(e)
@@ -11,6 +17,10 @@ modifyBtn.addEventListener('click', e => {
   showModify(e)
 })
 
+mobileMenu.addEventListener('click', e => {
+  mobileMenuClick(e)
+})
+
 
 function showCommentList(e) {
   commentList.classList.toggle('show')
@@ -18,4 +28,16 @@ function showCommentList(e) {
 
 function showModify(e) {
   userInfo.classList.toggle('show-modify')
+}
+
+function mobileMenuClick(e) {
+  const target = e.target
+  target.classList.add('clicked')
+  if (target === mobileContentsBtn) {
+    mobileInfoBtn.classList.remove('clicked')
+
+  } else if (target === mobileInfoBtn) {
+    mobileContentsBtn.classList.remove('clicked')
+    
+  }
 }
